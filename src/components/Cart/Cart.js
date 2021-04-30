a885452f1693ef40d91b240e230e7342bfd30c67
thenatural86 <philip.smalls86@gmail.com>
2021-04-30 11:55:26 -0400

    modal and styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 }].map((item) => (
@@ -11,7 +12,7 @@ const Cart = () => {
   )
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -21,7 +22,7 @@ const Cart = () => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 
