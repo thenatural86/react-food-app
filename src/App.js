9ea8f7c3c9c5ed0fdf6857b2e3da078f76c01a25
thenatural86 <philip.smalls86@gmail.com>
2021-04-30 20:10:32 -0400

    context created

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import { useState } from 'react'
 import Cart from './components/Cart/Cart'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
+import CartProvider from './context/CartProvider'
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false)
+  const [cartIsShown, setCartIsShown] = useState(true)
 
   const toggleCart = () => {
     setCartIsShown(!cartIsShown)
   }
 
   return (
-    <>
+    <CartProvider>
       {!cartIsShown && <Cart cartIsShown toggleCart={toggleCart} />}
       <Header toggleCart={toggleCart} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   )
 }
 
