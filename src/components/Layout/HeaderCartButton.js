014777f4992def78dd86eaa5aeea276575002ba9
thenatural86 <philip.smalls86@gmail.com>
2021-05-01 11:47:25 -0400

    reduce to total amount of cart items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import { useContext } from 'react'
+import CartContext from '../../context/cart-context'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = ({ toggleCart }) => {
+  const { items } = useContext(CartContext)
+
+  const numberOfItems = items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount
+  }, 0)
+
   return (
     <button className={classes.button} onClick={toggleCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   )
 }
