3b9141be73217d50238b27392b4008ab7d8b78a2
thenatural86 <philip.smalls86@gmail.com>
2021-05-01 17:44:59 -0400

    useEffect timer for bump animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import CartContext from '../../context/cart-context'
 
@@ -7,13 +7,30 @@ import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = ({ toggleCart }) => {
   const { items } = useContext(CartContext)
-
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const numberOfItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
   }, 0)
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={toggleCart}>
+    <button className={btnClasses} onClick={toggleCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
