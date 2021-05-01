f81875b45d0de6a37a461620ee8ab92da9e982fe
thenatural86 <philip.smalls86@gmail.com>
2021-05-01 16:21:48 -0400

    refactor add functionality

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -9,9 +9,27 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = [...state.items, action.payload]
     const updatedTotalAmount =
       state.totalAmount + action.payload.amount * action.payload.price
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.payload.id
+    )
+
+    const existingCartItem = state.items[existingCartItemIndex]
+    let updatedItems
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.payload.amount,
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      updatedItems = [...state.items, action.payload]
+    }
+
     return { ...state, items: updatedItems, totalAmount: updatedTotalAmount }
   }
   return defaultCartState
